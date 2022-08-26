78d419ae9165c563bacabcb268352b3f3985d702
Bruna Xavier <brunavxr@gmail.com>
2022-08-26 11:59:38 -0300

    Finalização da Classe Jogo

--- src/main/kotlin/encap/ReviewJogo.kt
+++ src/main/kotlin/encap/ReviewJogo.kt
@@ -7,38 +7,8 @@ abstract class ReviewJogo(
     var numCurtidas = 0
     var numDislikes = 0
 
-    open fun darNota() {
+    abstract fun darNota()
 
-        println("Dar nota de 1 a 5 para o jogo: ")
-        println("1 para ruim e 5 para gostei muito")
-        var nota = readln().toInt()
+    abstract fun curtir()
 
-        nota = notaDoJogo
-
-        if (nota in 1..5) {
-            println("Nota cadastrada com sucesso!")
-        } else {
-            println("Valor de $nota inválido! Digite um novo valor.")
-        }
-    }
-
-    open fun curtir() {
-
-        var count = 0
-
-        println("Você curtiu o jogo? ")
-        println("Sim ou Não")
-        var resp = readln()
-
-        if (resp == "Sim" && resp == "sim") {
-            count++
-            numCurtidas += count
-            println("Like: $numCurtidas")
-
-        } else {
-            count++
-            numDislikes += count
-            println("Dislike : $numDislikes")
-        }
-    }
 }
